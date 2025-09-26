8a7b74eda0fa82617c925a883a27a97b73945f58
Jaydeep karathiya <jaydeepcodecrew@gmail.com>
2025-09-26 16:43:10 +0530

    update sendMail api

--- app/routes/api.sendMail.jsx
+++ app/routes/api.sendMail.jsx
@@ -1,24 +1,29 @@
 import { json } from "@remix-run/node";
 import nodemailer from "nodemailer";
+import { cors } from "remix-utils/cors";
 import { authenticate } from "../shopify.server";
 
-
-const transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransporter({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
+
 export async function action({ request }) {
-   const { admin } = await authenticate.admin(request);
+  const { admin } = await authenticate.admin(request);
+  
   try {
     const data = await request.json();
     
     const { to = data.recipientEmail, subject, text, html = data.htmlTemplate } = data;
 
     if (!to) {
-      return json({ error: "Recipient email is required" }, { status: 400 });
+      return await cors(
+        request,
+        json({ error: "Recipient email is required" }, { status: 400 })
+      );
     }
 
     let info = await transporter.sendMail({
@@ -29,9 +34,15 @@ export async function action({ request }) {
       html: html || "<b>Hello world?</b>",
     });
 
-    return json({ success: true, messageId: info.messageId });
+    return await cors(
+      request,
+      json({ success: true, messageId: info.messageId })
+    );
   } catch (err) {
     console.error("Error sending mail:", err);
-    return json({ success: false, error: err.message }, { status: 500 });
+    return await cors(
+      request,
+      json({ success: false, error: err.message }, { status: 500 })
+    );
   }
 }
